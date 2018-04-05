7f85863821415304e77e615933b94dafedc70b4b
chanveer <chm026@gmail.com>
2018-04-05 19:17:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ restService.use(
 restService.use(bodyParser.json());
 
 
-restService.post("/user", function(req, res) {
-
+restService.post("/echo", function(req, res) {
   var speech =
-    req.body.result && req.body.result.parameters && req.body.result.parameters.date ? req.body.result.parameters.date: "Seems like some problem. Speak again.";
-    
-   
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
   return res.json({
-     speech: speech,
+    speech: speech,
     displayText: speech,
     source: "webhook-echo-sample"
   });
