ad561eeb916cfbae316764f2d77778b832da5273
chanveer <chm026@gmail.com>
2018-04-05 20:39:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,18 @@ restService.post("/check", function(req, res) {
   var jsonObj = require("./db.json");
   var string1 = "";
 	
-
+ if(req.body.result.parameters = "echoText"){
+  
+  for (var property1 in jsonObj.employess) {
+	 string1 = string1 + jsonObj.employess[property1].name + " : " + jsonObj.employess[property1].shift + " ";
+  }
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.echoText
+      ? req.body.result.parameters.echoText
+      : "Seems like some problem. Speak again.";
+   }
 	
   if(req.body.result.parameters = "leave"){
   
