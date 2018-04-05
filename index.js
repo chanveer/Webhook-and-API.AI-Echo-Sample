b0345a8588a67685fc65c7af284afa3e485b1efd
chanveer <chm026@gmail.com>
2018-04-05 16:45:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ restService.post("/user", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    for(var id = 0;id<empcount;id++){
-        speech: jsonObj.employess[id].name+' '+jsonObj.employess[id].shift,
-    }
+    //for(var id = 0;id<empcount;id++){
+        speech: jsonObj.employess[0].name+' '+jsonObj.employess[0].shift,
+    //}
     displayText: speech,
     source: "webhook-echo-sample"
   });
