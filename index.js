99c38af9844fbf28055214a9e74e902ec79818bc
chanveer <chm026@gmail.com>
2018-04-09 22:03:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ restService.post("/echo", function(req, res) {
 			      ? req.body.result.parameters.echoText
 			      : "Seems like some problem. Speak again.";
 			  return res.json({
-			    speech: speech,
+			    speech: output,
 			    displayText: speech,
 			    source: "webhook-echo-sample"
 			  });
