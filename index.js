e4fb908d934d1c385807e60a17be250b8b586317
chanveer <chm026@gmail.com>
2018-04-09 22:55:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,9 +32,11 @@ restService.post("/echo", function(req, res) {
 					for(var property2 in output[property1].schedule) {
 						
 						
+					var dateexcel = dateFormat(output[property1].schedule[property2].date, "yyyy-mm-dd");
+						
 					   // if(output[property1].schedule[property2].date  == req.body.result.parameters.date){
 						// string2 =   string2 + output[property1].firstname + "  "  +  output[property1].schedule[property2].starttime  + " : " + output[property1].schedule[property2].endtime + '\r\n';
-					    	 string2 = string2 + output[property1].schedule[property2].date + " " + req.body.result.parameters.date;
+					    	 string2 = string2 + dateexcel + " " + req.body.result.parameters.date;
 						
 						//}else{
 					    //	string2  = "No data found";
