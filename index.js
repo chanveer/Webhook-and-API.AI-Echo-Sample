7177aa2c0b92372c9b74d5d14baf188a09867460
chanveer <chm026@gmail.com>
2018-04-10 09:59:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ restService.post('/echo', function(req, res) {
 				for(var property1 in output) {
 					for(var property2 in output[property1].schedule) {
 						var dateexcel = dateFormat(output[property1].schedule[property2].date, "yyyy-mm-dd");
-							if((output[property1].schedule[property2].status   == 1)&&(dateexcel   == req.body.result.parameters.date1)){
+							if((output[property1].schedule[property2].status   == 1)&&(dateexcel   == req.body.result.parameters.date2)){
 								string1 =   string1 + output[property1].firstname + "  "  +  output[property1].schedule[property2].date + " " + output[property1].schedule[property2].status    + '\r\n';
 							}
 					}	
