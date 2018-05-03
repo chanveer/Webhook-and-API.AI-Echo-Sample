dd1c09378783a3645a229fb747d916493a9a1823
chanveer <chm026@gmail.com>
2018-05-03 19:23:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ restService.post('/echo', function(req, res) {
     console.log('=============' + req.body.result.action)
     switch (req.body.result.action) {
 		 case "Adddata":
-		     callApi1dataWrite().then((output) => {
+				var GoogleSpreadsheet = require('google-spreadsheet');
+			    var creds = require('./client_secret.json');
+				// Create a document object using the ID of the spreadsheet - obtained from its URL.
+				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
 			    
 				// Authenticate with the Google Spreadsheets API.
 				doc.useServiceAccountAuth(creds, function (err) {
@@ -30,63 +33,13 @@ restService.post('/echo', function(req, res) {
 				});
 				
                 return res.json({
-                    speech: string2,
+                    speech: "Data has been added",
                     source: 'webhook-echo-one',
 				});
-			});
+			
 		break;
     }
 });
-
-
-/*	
-function callApi1data () {
-  return new Promise((resolve, reject) => {
-		var GoogleSpreadsheet = require('google-spreadsheet');
-		var creds = require('./client_secret.json');
-		// Create a document object using the ID of the spreadsheet - obtained from its URL.
-		var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
- 
-		// Authenticate with the Google Spreadsheets API.
-		doc.useServiceAccountAuth(creds, function (err) {
- 
-			// Get all of the rows from the spreadsheet.
-			  doc.getRows(1, function (err1, rows1) {
-				   //console.log(row1);
-				  if (err1) {
-					console.log(err1);
-					reject(err1);
-				  }else {
-					  var data1 = rows1;
-					  
-					  var data = [];
-					  for (let row1 in data1) {
-							var inventory = {};
-							inventory.slno = data1[row1]['slno'];
-							inventory.productname = data1[row1]['productname'];
-							inventory.quantity = data1[row1]['quantity'];
-							data.push(inventory);
-							
-						
-					  }
-					  resolve(data);
-					  //console.log(data);
-				  }
-				
-			  });
-		});
-  })
-}
-*/
-
-function callApi1dataWrite () {
-  return new Promise((resolve, reject) => {
-		var GoogleSpreadsheet = require('google-spreadsheet');
-		var creds = require('./client_secret.json');
-		// Create a document object using the ID of the spreadsheet - obtained from its URL.
-		var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
- })
-}
  
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
