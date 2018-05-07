29213cc4d384b20b2da8d0829e761ab818cae1db
chanveer <chm026@gmail.com>
2018-05-08 03:14:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     console.log('=============' + req.body.result.action)
     switch (req.body.result.action) {
-		 case "Adddata":
+		case "Adddata":
 				var GoogleSpreadsheet = require('google-spreadsheet');
 			    var creds = require('./client_secret.json');
 				// Create a document object using the ID of the spreadsheet - obtained from its URL.
@@ -37,7 +37,7 @@ restService.post('/echo', function(req, res) {
 					
 				   });
 				});
-		var result = "Request has been captured in the sheet successfully.";		
+				var result = "Request has been captured in the sheet successfully.";		
                 return res.json({
                     speech: result,
                     source: 'webhook-echo-one',
@@ -68,7 +68,37 @@ restService.post('/echo', function(req, res) {
 					
 				   });
 				});
-		var result = "Request has been captured in the sheet successfully.";		
+				var result = "Request has been captured in the sheet successfully.";		
+                return res.json({
+                    speech: result,
+                    source: 'webhook-echo-one',
+				});
+		break;
+		
+		case "addproduct":
+		
+				var GoogleSpreadsheet = require('google-spreadsheet');
+			    var creds = require('./client_secret.json');
+				// Create a document object using the ID of the spreadsheet - obtained from its URL.
+				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
+				var result = "";
+				
+				// Authenticate with the Google Spreadsheets API.
+				doc.useServiceAccountAuth(creds, function (err) {
+					
+					//var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
+		 			var productname = req.body.result.contexts[0].parameters.any;
+					var quantity = req.body.result.contexts[0].parameters.number+" "+req.body.result.contexts[0].parameters['unit-weight-name'];
+					
+		 
+					doc.addRow(1, { PRODUCTNAME: productname,QUANTITY: quantity}, function(err) {
+					  if(err) {
+						console.log(err);
+					  }
+					
+				   });
+				});
+				var result = "Request has been captured in the sheet successfully.";		
                 return res.json({
                     speech: result,
                     source: 'webhook-echo-one',
