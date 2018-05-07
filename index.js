a4f5b4831389bf83bdd3918dacfe3f67573f9970
chanveer <chm026@gmail.com>
2018-05-07 13:15:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,29 +22,27 @@ restService.post('/echo', function(req, res) {
 				// Create a document object using the ID of the spreadsheet - obtained from its URL.
 				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
 				
-				var speech =
-				req.body.result &&
-				req.body.result.action &&
-				req.body.result.parameters &&
-				req.body.result.parameters['unit-weight-name']
-				  ? req.body.result.parameters['unit-weight-name']
-				  : "Can you please come again";
 			    
 				// Authenticate with the Google Spreadsheets API.
 				doc.useServiceAccountAuth(creds, function (err) {
 					
 					var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
 		 			//var quantity = req.body.result.parameters.number+":";
-		 
-					doc.addRow(1, { PRODUCTNAME: req.body.result.parameters.any,QUANTITY: quantity}, function(err) {
-					  if(err) {
-						console.log(err);
-					  }
-				   });
+					if(quantity != ''){
+						doc.addRow(1, { PRODUCTNAME: req.body.result.parameters.any,QUANTITY: quantity}, function(err) {
+						  if(err) {
+							console.log(err);
+						  }else{
+							var result = "Data has been added";
+						  }
+					   });
+					}else{
+						var result = "Can you come again";
+					}
 				});
 				
                 return res.json({
-                    speech: "Data has been added",
+                    speech: result,
                     source: 'webhook-echo-one',
 				});
 			
