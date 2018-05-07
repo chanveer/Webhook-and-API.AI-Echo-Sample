7bb68389227783fa3487c8cd94f77d4910151f67
chanveer <chm026@gmail.com>
2018-05-07 19:34:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ restService.use(bodyParser.json());
 restService.post('/echo', function(req, res) {
     console.log('=============' + req.body.result.action)
     switch (req.body.result.action) {
-		case "Adddata":
-		
-			var GoogleSpreadsheet = require('google-spreadsheet');
+		 case "Adddata":
+				var GoogleSpreadsheet = require('google-spreadsheet');
 			    var creds = require('./client_secret.json');
 				// Create a document object using the ID of the spreadsheet - obtained from its URL.
 				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
@@ -27,29 +26,38 @@ restService.post('/echo', function(req, res) {
 				// Authenticate with the Google Spreadsheets API.
 				doc.useServiceAccountAuth(creds, function (err) {
 					
-					//var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
-		 			//var quantity = req.body.result.parameters.number+":";
-		 			var sheet;
+					var sheet;
 					var dateFormat = require('dateformat');
 					var date = dateFormat(new Date(), "yyyy-mm-dd"); 
 					
-						doc.addWorksheet({
-						  title: 'INVENOTY-'+date
-						}, function(err, sheet) {
-							sheet.setHeaderRow(['productname', 'quantity']); //async
-						});
-			
-				
+					doc.getInfo(function(err, info) {
+						var cnt = 0;
+						for(var property1 in info.worksheets) {
+						  
+						    console.log(info.worksheets[property1].title);
+							cnt++;
+						  
+						}
+						if(info.worksheets[cnt-1].title == 'INVENOTY-'+date){
+							var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
+							doc.addRow(1, { PRODUCTNAME: req.body.result.parameters.any,QUANTITY: quantity}, function(err) {
+							  if(err) {
+								console.log(err);
+							  }
+						   });
+						}else{
+						  console.log("sheet is not there");
+						}
+					});
 				});
 				
-			return res.json({
-				speech: "Data has been added",
-				source: 'webhook-echo-one',
-			});
+                return res.json({
+                    speech: "Data has been added",
+                    source: 'webhook-echo-one',
+				});
 			
 		break;
-	}
-	
+    }
 });
  
 restService.listen(process.env.PORT || 8000, function() {
