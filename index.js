cbdb8f3654e241728d30d906b2e0dbd8975938af
chanveer <chm026@gmail.com>
2018-05-07 20:50:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,32 @@ restService.post('/echo', function(req, res) {
 				});
 			
 		break;
+		
+		case "addquantity":
+		
+				var GoogleSpreadsheet = require('google-spreadsheet');
+			    var creds = require('./client_secret.json');
+				// Create a document object using the ID of the spreadsheet - obtained from its URL.
+				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
+				
+				// Authenticate with the Google Spreadsheets API.
+				doc.useServiceAccountAuth(creds, function (err) {
+					
+					//var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
+		 			var quantity = req.body.context.parameters.number;
+		 
+					doc.addRow(1, { PRODUCTNAME: req.body.context.parameters.any,QUANTITY: quantity}, function(err) {
+					  if(err) {
+						console.log(err);
+					  }
+				   });
+				});
+				
+                return res.json({
+                    speech: "We have added the given  data",
+                    source: 'webhook-echo-one',
+				});
+		break;
     }
 });
  
