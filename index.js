8f5516e4385ab8fbecdcd022ab01b4fcf62fe689
chanveer <chm026@gmail.com>
2018-05-08 14:35:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,39 +17,111 @@ restService.post('/echo', function(req, res) {
     console.log('=============' + req.body.result.action)
     switch (req.body.result.action) {
 		case "Adddata":
-		    
 				var GoogleSpreadsheet = require('google-spreadsheet');
-				var creds = require('./client_secret.json');
+			    var creds = require('./client_secret.json');
 				// Create a document object using the ID of the spreadsheet - obtained from its URL.
 				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
-
+				var result = "";
+				
 				// Authenticate with the Google Spreadsheets API.
 				doc.useServiceAccountAuth(creds, function (err) {
 					
-					//var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
-					//var quantity = req.body.result.parameters.number+":";
-					var sheet;
-					var dateFormat = require('dateformat');
-					var date = dateFormat(new Date(), "yyyy-mm-dd"); 
-					
 					doc.getInfo(function(err, info) {
 						
-						doc.addWorksheet({
-						  title: 'Inventory-'+date
-						}, function(err, sheet) {
-							sheet.setHeaderRow(['productname', 'quantity']); //async
-						});
-					});	
+						var cnt = 0;
+						for(var property1 in info.worksheets) {
+							console.log(info.worksheets[property1].title);
+							cnt++;
+						  
+						}
+					
+						if(info.worksheets[cnt-1].title == 'Inventory-'+date){
+							
+							var productname = req.body.result.parameters.any;
+							var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
+							doc.addRow(cnt, { PRODUCTNAME: productname,QUANTITY: quantity}, function(err) {
+							  if(err) {
+								console.log(err);
+							  }
+							});
+							
+						}else{
+							
+							
+						}
+					
+					
+				   });
+				   
+				   
 				});
-				
-				var result = "Sheet Has been added successfully";		
+				var result = "Request has been captured in the sheet successfully.";		
                 return res.json({
                     speech: result,
                     source: 'webhook-echo-one',
 				});
 			
 		break;
-
+		
+		case "addquantity":
+		
+				var GoogleSpreadsheet = require('google-spreadsheet');
+			    var creds = require('./client_secret.json');
+				// Create a document object using the ID of the spreadsheet - obtained from its URL.
+				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
+				var result = "";
+				
+				// Authenticate with the Google Spreadsheets API.
+				doc.useServiceAccountAuth(creds, function (err) {
+					
+					//var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
+		 			var productname = req.body.result.contexts[0].parameters.product;
+					var quantity = req.body.result.contexts[0].parameters.number+" "+req.body.result.contexts[0].parameters['unit-weight-name'];
+					
+		 
+					doc.addRow(1, { PRODUCTNAME: productname,QUANTITY: quantity}, function(err) {
+					  if(err) {
+						console.log(err);
+					  }
+					
+				   });
+				});
+				var result = "Request has been captured in the sheet successfully.";		
+                return res.json({
+                    speech: result,
+                    source: 'webhook-echo-one',
+				});
+		break;
+		
+		case "addproduct":
+		
+				var GoogleSpreadsheet = require('google-spreadsheet');
+			    var creds = require('./client_secret.json');
+				// Create a document object using the ID of the spreadsheet - obtained from its URL.
+				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
+				var result = "";
+				
+				// Authenticate with the Google Spreadsheets API.
+				doc.useServiceAccountAuth(creds, function (err) {
+					
+					//var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
+		 			var productname = req.body.result.contexts[0].parameters.any;
+					var quantity = req.body.result.contexts[0].parameters.number+" "+req.body.result.contexts[0].parameters['unit-weight-name'];
+					
+		 
+					doc.addRow(1, { PRODUCTNAME: productname,QUANTITY: quantity}, function(err) {
+					  if(err) {
+						console.log(err);
+					  }
+					
+				   });
+				});
+				var result = "Request has been captured in the sheet successfully.";		
+                return res.json({
+                    speech: result,
+                    source: 'webhook-echo-one',
+				});
+		break;
     }
 });
  
