59fa053f93c812be6e6680cf3a674905fe931c5b
chanveer <chm026@gmail.com>
2018-05-08 12:26:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,93 +17,49 @@ restService.post('/echo', function(req, res) {
     console.log('=============' + req.body.result.action)
     switch (req.body.result.action) {
 		case "Adddata":
-				var GoogleSpreadsheet = require('google-spreadsheet');
-			    var creds = require('./client_secret.json');
-				// Create a document object using the ID of the spreadsheet - obtained from its URL.
-				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
-				var result = "";
-				
-				// Authenticate with the Google Spreadsheets API.
-				doc.useServiceAccountAuth(creds, function (err) {
-					
-					var productname = req.body.result.parameters.any;
-					var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
-		 	
-		 
-					doc.addRow(1, { PRODUCTNAME: productname,QUANTITY: quantity}, function(err) {
-					  if(err) {
-						console.log(err);
-					  }
-					
-				   });
-				});
-				var result = "Request has been captured in the sheet successfully.";		
-                return res.json({
+		    var GoogleSpreadsheet = require('google-spreadsheet');
+		var creds = require('./client_secret.json');
+		// Create a document object using the ID of the spreadsheet - obtained from its URL.
+		var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
+
+		// Authenticate with the Google Spreadsheets API.
+		doc.useServiceAccountAuth(creds, function (err) {
+			
+			
+			var GoogleSpreadsheet = require('google-spreadsheet');
+			var creds = require('./client_secret.json');
+			// Create a document object using the ID of the spreadsheet - obtained from its URL.
+			var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
+
+			// Authenticate with the Google Spreadsheets API.
+			doc.useServiceAccountAuth(creds, function (err) {
+
+				//var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
+				//var quantity = req.body.result.parameters.number+":";
+				var sheet;
+				var dateFormat = require('dateformat');
+				var date = dateFormat(new Date(), "yyyy-mm-dd"); 
+
+				doc.getInfo(function(err, info) {
+
+					doc.addWorksheet({
+					  title: 'Inventory-'+date
+					}, function(err, sheet) {
+						sheet.setHeaderRow(['productname', 'quantity']); //async
+					});
+
+				});	
+
+
+			});		
+		    var result = "Request has been captured in the sheet successfully.";		
+                    return res.json({
                     speech: result,
                     source: 'webhook-echo-one',
 				});
 			
 		break;
-		
-		case "addquantity":
-		
-				var GoogleSpreadsheet = require('google-spreadsheet');
-			    var creds = require('./client_secret.json');
-				// Create a document object using the ID of the spreadsheet - obtained from its URL.
-				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
-				var result = "";
-				
-				// Authenticate with the Google Spreadsheets API.
-				doc.useServiceAccountAuth(creds, function (err) {
-					
-					//var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
-		 			var productname = req.body.result.contexts[0].parameters.product;
-					var quantity = req.body.result.contexts[0].parameters.number+" "+req.body.result.contexts[0].parameters['unit-weight-name'];
-					
-		 
-					doc.addRow(1, { PRODUCTNAME: productname,QUANTITY: quantity}, function(err) {
-					  if(err) {
-						console.log(err);
-					  }
-					
-				   });
-				});
-				var result = "Request has been captured in the sheet successfully.";		
-                return res.json({
-                    speech: result,
-                    source: 'webhook-echo-one',
-				});
-		break;
-		
-		case "addproduct":
-		
-				var GoogleSpreadsheet = require('google-spreadsheet');
-			    var creds = require('./client_secret.json');
-				// Create a document object using the ID of the spreadsheet - obtained from its URL.
-				var doc = new GoogleSpreadsheet('19z_cDmfUprmx-xKEynMeMvu0SQNua_dEUMB2SHwDn6w');
-				var result = "";
-				
-				// Authenticate with the Google Spreadsheets API.
-				doc.useServiceAccountAuth(creds, function (err) {
-					
-					//var quantity = req.body.result.parameters.number+" "+req.body.result.parameters['unit-weight-name'];
-		 			var productname = req.body.result.contexts[0].parameters.any;
-					var quantity = req.body.result.contexts[0].parameters.number+" "+req.body.result.contexts[0].parameters['unit-weight-name'];
-					
-		 
-					doc.addRow(1, { PRODUCTNAME: productname,QUANTITY: quantity}, function(err) {
-					  if(err) {
-						console.log(err);
-					  }
-					
-				   });
-				});
-				var result = "Request has been captured in the sheet successfully.";		
-                return res.json({
-                    speech: result,
-                    source: 'webhook-echo-one',
-				});
-		break;
+
     }
 });
  
